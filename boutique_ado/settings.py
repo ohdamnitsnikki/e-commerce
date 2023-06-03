cfcdbe7f7456efa20934231869f6724b6dfaefd5
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-03 10:16:36 +0000

    add html in bag for mobile view

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-ohdamnitsnikk-ecommerce-nidpref5axe.ws-eu97.gitpod.io', 'localhost']
 
