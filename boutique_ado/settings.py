2f5cc0ea0ba94b3e11a62e6d29871d97576470ac
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-06-07 20:47:20 +0000

    change allowed host

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ohdamnitsnikk-ecommerce-nidpref5axe.ws-eu97.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['8000-ohdamnitsnikk-ecommerce-nidpref5axe.ws-eu98.gitpod.io', 'localhost']
 
 
 # Application definition
